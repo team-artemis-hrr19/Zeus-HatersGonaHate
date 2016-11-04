e38561eea20e967d773ed2edbcd00fcd0625c332
jonathan.deng <dengjonathan@gmail.com>
2016-11-04 17:39:21 -0400

    updated rec users component to delete user and load random on x

--- client/app/newsfeed/newsfeed.js
+++ client/app/newsfeed/newsfeed.js
@@ -1,6 +1,8 @@
-angular.module('zeus.newsfeed', ['rx'])
-  .controller('newsFeedController', function ($scope, $http) {
-    $scope.recUsers = [];
+angular.module('zeus.newsfeed', [])
+  .controller('newsFeedController', function ($scope, $http, EventConverter) {
+    $scope.users = [];
+    $scope.recUsers = []
+    $scope.events = [];
     $scope.value = 'jon';
 
     const userStream = Rx.Observable.fromPromise(
@@ -9,10 +11,45 @@ angular.module('zeus.newsfeed', ['rx'])
         url: '/user'
       })
     );
-    $scope.$createObservableFunction('click')
-      .map(() => $scope.recUsers)
-      .flatMapLatest(userStream)
-      .subscribe(results => {
-        $scope.recUsers = results.data.slice(3);
-      });
+
+    userStream
+      .safeApply($scope, users => {
+        $scope.users = users.data;
+        $scope.recUsers = $scope.users.slice(0, 3);
+      })
+      .subscribe();
+
+    $scope.closeUser = function (username) {
+      console.log('X clicked', username);
+      const random = $scope.users[Math.floor(Math.random() * $scope.users.length)];
+      const index = $scope.recUsers.map(user => user.username)
+        .indexOf(username);
+      $scope.recUsers[index] = random;
+    };
+
+    // const suggestedUser1Stream = userStream
+    //   .map(users => {
+    //     console.log(users);
+    //     return 
+    //   });
+
+    // $scope.$createObservableFunction('click')
+    //   .map(() => $scope.recUsers)
+    //   .flatMapLatest(userStream)
+    //   .subscribe(results => {
+    //     $scope.recUsers = results.data;
+    //   });
+
+    const eventStream = Rx.Observable.fromPromise(
+      $http({
+        method: 'GET',
+        url: '/event'
+      })
+    );
+
+    eventStream
+      .safeApply($scope, function (events) {
+        $scope.events = events.data;
+      })
+      .subscribe();
   });

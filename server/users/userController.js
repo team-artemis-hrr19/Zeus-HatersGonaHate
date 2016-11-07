c8c742eecbc6971d5becae137c1b118deb196ca4
jonathan.deng <dengjonathan@gmail.com>
2016-11-06 21:27:27 -0500

    fixed liking/unliking movies

--- server/users/userController.js
+++ server/users/userController.js
@@ -96,9 +96,10 @@ module.exports = {
   addToUserLists: function (req, res, next) {
     var id = req.user.sub;
     var type = req.body.type;
-    var payload = req.body.payload;
+    var payload = req.body;
     console.log(type, payload, id);
     helpers.addToListByType(id, payload, type, res);
+    res.sendStatus(200);
   },
 
   //Removes data from the list based on the type param ('favorites', 'watched', 'currentlyWatching')
